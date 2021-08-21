46dbcb9c54ff6ff5008ecb7f01da9af7c815b780
Valdemar Grange <randomvald0069@gmail.com>
2021-08-21 19:06:21 +0200

    it works first try... how?

--- src/main/scala/tray/BlobStore.scala
+++ src/main/scala/tray/BlobStore.scala
@@ -14,11 +14,16 @@ import cats.implicits._
 import org.http4s._
 import org.http4s.headers._
 import cats.effect.kernel.Resource
+import cats.data.NonEmptyList
 
 trait BlobStore[F[_]] {
-  def putBlob(path: StoragePath, contentType: MediaType, bytes: Array[Byte]): F[Unit]
+  def putBlob(path: StoragePath, bytes: Array[Byte]): F[Unit]
 
-  def putPipe(path: StoragePath, contentType: MediaType, length: Long): Pipe[F, Byte, Unit]
+  //triggers chunked transfer encoding
+  def putPipe(path: StoragePath, length: Long): Pipe[F, Byte, Unit]
+
+  //enables resumed uploads
+  def putResumable(path: StoragePath, chunkSize: Int = 512 * 1024): Pipe[F, Byte, (Throwable, Location, Long)]
 
   def getBlob(path: StoragePath): Resource[F, Stream[F, Byte]]
 }
@@ -33,13 +38,14 @@ object BlobStore {
             Concurrent[F].raiseError(new TrayError.HttpStatusError(s"tray response had bad status code: $res\n$body"))
           }
 
-      def putBlob(path: StoragePath, contentType: MediaType, bytes: Array[Byte]): F[Unit] =
-        Stream.chunk[F, Byte](Chunk.array(bytes))
-          .through(putPipe(path, contentType, bytes.length.toLong))
+      def putBlob(path: StoragePath, bytes: Array[Byte]): F[Unit] =
+        Stream
+          .chunk[F, Byte](Chunk.array(bytes))
+          .through(putPipe(path, bytes.length.toLong))
           .compile
           .drain
 
-      def putPipe(path: StoragePath, contentType: MediaType, length: Long): Pipe[F, Byte, Unit] = { stream =>
+      def putPipe(path: StoragePath, length: Long): Pipe[F, Byte, Unit] = { stream =>
         val eff =
           for {
             headers <- auth.getHeader
@@ -49,7 +55,8 @@ object BlobStore {
                   method = Method.POST,
                   uri = Endpoints.upload / "b" / path.bucket / "o" +? ("name", path.path) +? ("uploadType", "media"),
                   body = stream,
-                  headers = Headers(`Content-Type`(contentType), `Content-Length`(length)) ++ headers
+                  headers =
+                    Headers(`Content-Type`(MediaType.application.`octet-stream`), `Content-Length`(length)) ++ headers
                 )
               )
               .use(failOnNonSuccess)
@@ -58,6 +65,104 @@ object BlobStore {
 
         Stream.eval(eff)
       }
+
+      def putByteRange(uri: Uri, offset: Long, chunk: Chunk[Byte]): F[Either[Throwable, Long]] = {
+        val req = Request[F](
+          method = Method.PUT,
+          uri = uri,
+          headers = Headers(`Content-Length`(chunk.size), `Content-Range`(offset, offset + chunk.size))
+        ).withEntity(chunk)
+
+        val effect: F[Either[Throwable, Long]] = client.run(req).use[Either[Throwable, Long]] { resp =>
+          //Resume Incomplete https://cloud.google.com/storage/docs/performing-resumable-uploads#resume-upload
+          val parsed: Either[Throwable, Long] = if (resp.status.code == 308) {
+            val rh: Option[Range] = resp.headers.get[Range]
+            val outE: Either[Throwable, Long] = rh match {
+              case None => Left(new Exception(s"missing range header in response: $resp"))
+              case Some(rng) =>
+                val maxed = rng.ranges.toList
+                  .flatMap(_.second.toList)
+                  .maxByOption(identity)
+
+                maxed match {
+                  case None        => Left(new Exception(s"range header had no end ranges in response: $resp"))
+                  case Some(value) => Right(value)
+                }
+            }
+            outE
+          } else if (resp.status == Status.Ok || resp.status == Status.Created) {
+            Right(chunk.size + offset)
+          } else {
+            Left(new Exception(s"got unexpected result from api with response: $resp"))
+          }
+
+          Concurrent[F].pure(parsed)
+        }
+
+        effect.attempt
+          .map(_.flatten)
+      }
+
+      def resumeFrom(uri: Uri, offset: Long, chunkSize: Int, stream: Stream[F, Byte]): Stream[F, (Throwable, Long)] =
+        stream.pull
+          .unconsN(chunkSize)
+          .flatMap {
+            case None => Pull.done
+            case Some((x: Chunk[Byte], xs: Stream[F, Byte])) =>
+              val placed: F[Either[Throwable, Long]] = putByteRange(uri, offset, x)
+
+              val outStream: Stream[F, (Throwable, Long)] = Stream.eval(placed).flatMap {
+                case Left(err) => Stream((err, offset))
+                case Right(newOffest) =>
+                  val toDrop = newOffest - offset
+
+                  val chunksThatServerDidntEat: Chunk[Byte] =
+                    if (toDrop == x.size) {
+                      Chunk.empty
+                    } else {
+                      x.drop(toDrop.toInt)
+                    }
+
+                  resumeFrom(
+                    uri,
+                    offset = offset + chunkSize - chunksThatServerDidntEat.size,
+                    chunkSize = chunkSize,
+                    stream = xs.cons(chunksThatServerDidntEat)
+                  )
+              }
+
+              outStream.pull.echo
+          }
+          .stream
+
+      def putResumable(path: StoragePath, chunkSize: Int = 512 * 1024): Pipe[F, Byte, (Throwable, Location, Long)] =
+        stream => {
+          val putLoc: F[Location] =
+            for {
+              headers <- auth.getHeader
+              resp <- client
+                .run(
+                  Request[F](
+                    method = Method.POST,
+                    uri =
+                      Endpoints.upload / "b" / path.bucket / "o" +? ("name", path.path) +? ("uploadType", "resumable"),
+                    headers = Headers(`Content-Type`(MediaType.application.`octet-stream`)) ++ headers
+                  )
+                )
+                .use(x => failOnNonSuccess(x))
+              loc <- Concurrent[F].fromOption(
+                resp.headers.get[`Location`],
+                new Exception(s"failed to find location header in resumable upload")
+              )
+            } yield loc
+
+          val putInto: Stream[F, (Throwable, Location, Long)] =
+            Stream
+              .eval(putLoc)
+              .flatMap(loc => resumeFrom(loc.uri, 0, chunkSize, stream).map { case (t, o) => (t, loc, o) })
+
+          putInto
+        }
 
       //https://cloud.google.com/storage/docs/json_api/v1/objects/get
       def getBlob(path: StoragePath): Resource[F, Stream[F, Byte]] =
--- src/test/scala/tray/InsertGetTest.scala
+++ src/test/scala/tray/InsertGetTest.scala
@@ -24,7 +24,7 @@ class InsertGetTest extends CatsEffectSuite {
   val sp = StoragePath(element, "os-valdemar")
 
   test(s"should insert an object by name $element") {
-    clientFixture().map(_.putBlob(sp, MediaType.application.`octet-stream`, element.getBytes(StandardCharsets.UTF_8)))
+    clientFixture().map(_.putBlob(sp, element.getBytes(StandardCharsets.UTF_8)))
   }
 
   test(s"should get the inserted object by name $element") {
@@ -32,5 +32,12 @@ class InsertGetTest extends CatsEffectSuite {
       .flatMap(_.getBlob(sp).use(_.compile.to(Array)))
       .map(bytes => new String(bytes, StandardCharsets.UTF_8))
     assertIO(stringF, element)
+  }
+
+  test(s"should upload resumable") {
+    clientFixture().flatMap{ bs =>
+      val bytes = fs2.Stream(element.getBytes().toList : _*).lift[IO]
+      bytes.through(bs.putResumable(StoragePath("der er hest i den", "os-valdemar"))).compile.drain
+    }
   }
 }
